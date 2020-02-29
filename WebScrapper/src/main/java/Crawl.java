55610738d4c135fd64fc35cc2f605edfb246ade7
GlendonK <36408879+GlendonK@users.noreply.github.com>
2020-02-29 23:37:40 +0800

    Add files via upload

--- WebScrapper/src/main/java/Crawl.java
+++ WebScrapper/src/main/java/Crawl.java
@@ -1,6 +1,8 @@
+import com.google.gson.internal.$Gson$Preconditions;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Crawl {
 
@@ -11,21 +13,41 @@ public class Crawl {
 
     public void getWords(String url)
     {
-
-
         try
         {
             Document doc = Jsoup.connect(url).get();
             //System.out.println(doc.outerHtml());
             for (Element row : doc.select("h3.teaser__heading"))
             {
+
                 System.out.println(row.select("a.teaser__title"));
+
+
             }
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
+    }
+
+    public void getHeader(String url){
+        try
+        {
+
+            Document html = Jsoup.parse(Jsoup.connect(url).get().html());
+            //Elements header = html.getElementsByTag("h3");
+            Element link = html.select("h3").first();
+            
+
+            System.out.println("Outer HTML: " + link.outerHtml());
+            System.out.println("Inner HTML: " + link.html());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
 
--- WebScrapper/src/main/java/Main.java
+++ WebScrapper/src/main/java/Main.java
@@ -7,12 +7,15 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String url = "https://www.channelnewsasia.com/news/topic/stock-market";
+        String url = "https://www.channelnewsasia.com/news/topic/stock-market"; // other than first time... url will be url = page.getNextUrl(url)
         Crawl crawl = new Crawl();
         crawl.getWords(url);
+        crawl.getHeader(url);
 
         Page page = new Page();
         System.out.println(page.getNextUrl(url));
+
+
 
 
     }
